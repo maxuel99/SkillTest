d9ea059ec873e9d82c6cd8de28a1bdf48f49030a
maxuel99 <massimocaser@gmail.com>
2022-11-03 18:22:38 +0100

    add feature/LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import "./App.css";
+import BoxCard from "./Components/LoginPage/BoxCard";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<BoxCard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
